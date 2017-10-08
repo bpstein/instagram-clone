ea37e6ab9afbb2c49d17f4506d8a725e32d9e7b5
Ben Stein <bumstain@gmail.com>
2017-10-08 17:21:12 +0900

    User cannot post without uploading an image

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -10,4 +10,12 @@ feature 'Creating posts' do
     expect(page).to have_content('nom nom')
     expect(page).to have_css("img[src*='taco.jpg']")
   end
+
+  scenario 'need an image to create a post' do 
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'I will not upload a pic'
+    click_button 'Create Post'
+    expect(page).to have_content('You must upload an image!')
+  end
 end
